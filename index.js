cc15607a268348407bac0b384a2fa349fa9f53e4
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-03-13 15:14:41 +0300

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Mongoose gives us a simple API to work with mongoDB
+const mongoose = require('mongoose'); // return a mongoose object
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+// This reference to mongoDB that we install on this machine. When you wanna deploy your application to production environment, you are going to have different connection string for the production environment. You can set various configuration for different environments using config module.
+// mongoose.connect() returns promise object.
